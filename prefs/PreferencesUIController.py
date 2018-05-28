6047ddce09cc7e950810eeff9e94d79b8ebfccb2
Darwin Sardual <darwin.sardual@gmail.com>
2018-05-28 11:34:52 +0800

    clean whitespaces

--- prefs/PreferencesUIController.py
+++ prefs/PreferencesUIController.py
@@ -3,20 +3,20 @@ from PreferencesUI import PreferencesUI
 from Config import Config
 
 class PreferencesUIController:
-    
+
     def __init__(self):
-        
+
         self.preferencesUI = PreferencesUI()
         self.pref = Config.getConfig()
         self.__initValues()
         self.__initSignals()
-        
+
         self.preferencesUI.exec_()
 
     def __initValues(self):
         self.preferencesUI.widthLine.setText(str(self.pref["dimensions"]["width"]))
         self.preferencesUI.heightLine.setText(str(self.pref["dimensions"]["height"]))
-        
+
         if self.pref["mode"] == "mobile":
             self.preferencesUI.mobileRadioButton.setChecked(True)
             self.mode = self.pref["mode"]
@@ -29,22 +29,22 @@ class PreferencesUIController:
         self.preferencesUI.cancelButton.clicked.connect(self.preferencesUI.close)
         self.preferencesUI.mobileRadioButton.toggled.connect(lambda: self.modeToggled(self.preferencesUI.mobileRadioButton))
         self.preferencesUI.desktopRadioButton.toggled.connect(lambda: self.modeToggled(self.preferencesUI.desktopRadioButton))
-        
+
     def saveButtonClicked(self):
         newPref = {}
         newPref["dimensions"] = {}
 
         newPref["dimensions"]["width"] = int(self.preferencesUI.widthLine.text())
         newPref["dimensions"]["height"] = int(self.preferencesUI.heightLine.text())
-        
+
         newPref["mode"] = self.mode
-        
+
         Config.createPreferencesConfig(newPref)
         self.preferencesUI.close()
-        
-        
+
+
     def modeToggled(self, button):
-        
+
         if button == self.preferencesUI.mobileRadioButton:
             if button.isChecked():
                 self.mode = "mobile"
